e027d8d95a785b6d580df00923c949f253de4c8e
Jongu Yu <yujongu@gmail.com>
2019-05-22 13:18:44 +0900

    leetCode problem 26

---
+++ leet26.java
@@ -0,0 +1,12 @@
+public int removeDuplicates(int[] nums) {
+  int len = 0;
+
+  for(int i = 1; i < nums.length; i++){
+      if(nums[len] != nums[i]){
+          len++;
+          nums[len] = nums[i];
+      }
+  }
+  len++;
+  return len;
+}
