1790071ad830d3a45c25fe9bb4633978fd6f8f0a
Jongu Yu <yujongu@gmail.com>
2019-05-24 17:10:30 +0900

    3 sum with different way that didn't work

--- leet15.java
+++ leet15.java
@@ -1,61 +1,97 @@
-public List<List<Integer>> threeSum(int[] nums) {
-    List<List<Integer>> ans = new ArrayList<>();
+class problem {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
 
 
-    //sort
-    for(int i = 0; i < nums.length - 1; i++){
-        for(int j = 0; j < nums.length - i - 1; j++){
-            if (nums[j] > nums[j + 1]){
-                int temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-            }
-        }
-    }
-
-
-    int count = 0;
-    for (int i = 0; i < nums.length - 1; i++){
-        for (int j = i + 1; j < nums.length; j++){
-            int sum = nums[i] + nums[j];
-
-            for(int k = nums.length - 1; k >= j + 1; k--){
-                if (nums[k] + sum == 0){
-                    ans.add(new ArrayList<>());
-                    ans.get(count).add(nums[i]);
-                    ans.get(count).add(nums[j]);
-                    ans.get(count).add(nums[k]);
-                    count++;
+        //sort
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = 0; j < nums.length - i - 1; j++){
+                if (nums[j] > nums[j + 1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
-    }
 
-    for (int i = 0; i < ans.size() - 1; i++){
-        for (int j = i + 1; j < ans.size(); j++){
-            if (ans.get(i).equals(ans.get(j))){
-                ans.get(j).add(-1);
+        int zeroIndex = findZeroIndex(nums);
+
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                int sum = nums[i] + nums[j];
+
+
+//                if (sum > 0){
+//                    for (int k = 0; k < zeroIndex; k++){
+//                        if (nums[k] + sum == 0){
+//                            ans.add(new ArrayList<>());
+//                            ans.get(count).add(nums[i]);
+//                            ans.get(count).add(nums[j]);
+//                            ans.get(count).add(nums[k]);
+//                            count++;
+//                        }
+//                    }
+//                } else {
+//                    for (int k = nums.length - 1; k >= zeroIndex; k--){
+//                        if (nums[k] + sum == 0){
+//                            ans.add(new ArrayList<>());
+//                            ans.get(count).add(nums[i]);
+//                            ans.get(count).add(nums[j]);
+//                            ans.get(count).add(nums[k]);
+//                            count++;
+//                        }
+//                    }
+//                }
+
+                for(int k = nums.length - 1; k >= j + 1; k--){
+                    if (nums[k] + sum == 0){
+                        ans.add(new ArrayList<>());
+                        ans.get(count).add(nums[i]);
+                        ans.get(count).add(nums[j]);
+                        ans.get(count).add(nums[k]);
+                        count++;
+                    }
+                }
             }
         }
-    }
 
-    List<List<Integer>> temp = new ArrayList<>();
-
-
-    for (int i = 0; i < ans.size(); i++){
-        if (ans.get(i).size() < 4){
-            temp.add(ans.get(i));
+        for (int i = 0; i < ans.size() - 1; i++){
+            for (int j = i + 1; j < ans.size(); j++){
+                if (ans.get(i).equals(ans.get(j))){
+                    ans.get(j).add(-1);
+                }
+            }
         }
+
+        List<List<Integer>> temp = new ArrayList<>();
+
+
+        for (int i = 0; i < ans.size(); i++){
+            if (ans.get(i).size() < 4){
+                temp.add(ans.get(i));
+            }
+        }
+
+//        for (int i = 0; i < temp.size(); i++){
+//            for (int j = 0; j < 3; j++){
+//                System.out.print(temp.get(i).get(j) + ", ");
+//            }
+//            System.out.println();
+//        }
+
+        ans = temp;
+
+        return ans;
     }
 
-  //        for (int i = 0; i < temp.size(); i++){
-  //            for (int j = 0; j < 3; j++){
-  //                System.out.print(temp.get(i).get(j) + ", ");
-  //            }
-  //            System.out.println();
-  //        }
+    public int findZeroIndex(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
 
-    ans = temp;
-
-    return ans;
 }
